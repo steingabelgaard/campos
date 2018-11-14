e26f1bfb2a314e53f8804d900b18b38cbba11545
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2018-11-14 16:38:22 +0100

    Customized HR Recruitment Form

---
+++ campos_hr_recruitment/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Stein & Gabelgaard ApS
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Campos Hr Recruitment',
+    'description': """
+        CampOS HR Recruitment modifications""",
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Stein & Gabelgaard ApS',
+    'website': 'www.steingabelgaard.dk',
+    'depends': [
+    ],
+    'data': [
+        'templates/recruitment_template.xml',
+    ],
+    'demo': [
+    ],
+}

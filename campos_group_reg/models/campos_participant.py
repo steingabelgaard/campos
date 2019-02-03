d44fcf051ef19fabaa473b671201bc7bdccdf20e
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2019-02-03 16:48:50 +0100

    Access rigths for partipants creation

--- campos_group_reg/models/campos_participant.py
+++ campos_group_reg/models/campos_participant.py
@@ -3,6 +3,8 @@
 
 from odoo import api, fields, models, _
 
+import logging
+_logger = logging.getLogger(__name__)
 
 class CamposParticipant(models.Model):
 
@@ -13,8 +15,14 @@ class CamposParticipant(models.Model):
     _order = 'name'
     _mail_post_access = 'read'
 
+    def _get_group_reg_id(self):
+        if self.env.user.partner_id.parent_id.scoutgroup:
+            group_reg = self.env['campos.group.reg'].sudo().search([('partner_id', '=', self.env.user.partner_id.parent_id.id)])
+            return group_reg.id
+        return False
+    
     partner_id = fields.Many2one('res.partner', 'Member', delegate=True, required=True, ondelete='restrict', index=True, auto_join=True)
-    group_reg_id = fields.Many2one('campos.group.reg', string='Group')
+    group_reg_id = fields.Many2one('campos.group.reg', string='Group', default=_get_group_reg_id)
     state = fields.Selection([('draft', 'Draft'),
                               ('confirmed', 'Confirmed'),
                               ('cancelled', 'Cancelled'),
@@ -29,11 +37,16 @@ class CamposParticipant(models.Model):
     def _update_camp_days(self):
         days = self.env['campos.camp.day'].search([('campday', '>=', self.arrival_date_id.campday), ('campday', '<=', self.depature_date_id.campday)])
         if days:
-            self.camp_day_ids = days
+            self.suspend_security().camp_day_ids = days
                                                     
     @api.model
     def create(self, vals):
-        res = super(CamposParticipant, self).create(vals)
+        if 'group_reg_id' not in vals:
+            vals['group_reg_id'] = self._get_group_reg_id()
+        vals['parent_id'] = self.env['campos.group.reg'].browse(vals['group_reg_id']).partner_id.id
+        vals['participant'] = True
+        _logger.info('CREATE: %s', vals)    
+        res = super(CamposParticipant, self.sudo()).create(vals)
         if 'arrival_date_id' in vals and 'depature_day_id' in vals:
             self._update_camp_days()
         return res

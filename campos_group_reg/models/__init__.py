1c03c7d181e96b7ee35e3c20850be9c1e22e1a91
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2019-07-31 13:05:50 +0200

    Disable frontend for now (no website-cms modules)

--- campos_group_reg/models/__init__.py
+++ campos_group_reg/models/__init__.py
@@ -2,13 +2,13 @@ from . import campos_group_reg
 from . import campos_age_group
 from . import campos_camp_period
 from . import campos_prereg_age_period
-from . import cms_form_campos_group_reg
+# from . import cms_form_campos_group_reg
 from . import res_partner
 from . import campos_scout_org
-from . import account_form
+# from . import account_form
 from . import campos_participant
 from . import campos_camp_day
-from . import cms_form_campos_participant
+# from . import cms_form_campos_participant
 
 # from . import campos_participant_day
 from . import campos_accommodation_type
